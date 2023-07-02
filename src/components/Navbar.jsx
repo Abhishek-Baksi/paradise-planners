aec2e414c1682bbb12ca182660bbd96686f7efae
Abhishek-Baksi <abhishekbaksi1997@gmail.com>
2023-07-02 05:49:24 +0530

    ""

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,20 @@ const Navbar = () => {
   };
 
   return (
-    <div className="absolute w-full flex justify-between p-4 items-center">
+    <div className="absolute w-full flex justify-between p-4 items-center ">
       <h1 className="text-white font-bold text-2xl z-20">ParadisePlanners</h1>
       {/* <HiMenuAlt3 onClick={handleNavBtn} size={25} className="z-20 text-white cursor-pointer" /> */}
 
-      {nav ? <HiOutlineXCircle onClick={handleNavBtn} size={25} className={`z-20 text-white cursor-pointer `} /> : <HiMenuAlt3 onClick={handleNavBtn} size={25} className={`z-20 text-white cursor-pointer`} />}
+      {/* <ul className="flex flex-row fixed w-full h-full justify-center items-center text-white 
+       md:hidden z-100">
+        <li className="font-bol text-3xl p-8">Home</li>
+        <li className="font-bol text-3xl p-8">Destination</li>
+        <li className="font-bol text-3xl p-8">Reservation</li>
+        <li className="font-bol text-3xl p-8">Amenities</li>
+        <li className="font-bol text-3xl p-8">Rooms</li>
+      </ul> */}
+
+      {nav ? <HiOutlineXCircle onClick={handleNavBtn} size={25} className={`z-20 text-white cursor-pointer md:hidden`} /> : <HiMenuAlt3 onClick={handleNavBtn} size={25} className={`z-20 text-white cursor-pointer md:hidden`} />}
       <div className={nav ? "ease-in duration-300 fixed text-gray-300 top-0 left-0 w-full h-screen bg-black bg-opacity-90 px-4 py-7 flex-col z-10 backdrop-filter backdrop-blur-sm" : "absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10"}>
         <ul className="flex flex-col fixed w-full h-full  items-center justify-center">
           <li className="font-bol text-3xl p-8">Home</li>
